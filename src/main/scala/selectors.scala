dae942c8f568226160bef6a525bdf981a00fe860
Christian Krause <kizkizzbangbang@googlemail.com>
2012-06-14 12:57:05 +0200

    fixed GPL header

--- src/main/scala/selectors.scala
+++ src/main/scala/selectors.scala
@@ -10,13 +10,13 @@
  *  This file is part of 'scalevalgo'.                                      *
  *                                                                          *
  *  This project is free software: you can redistribute it and/or modify    *
- *  it under the terms of the GNU General Public License individuals published by    *
+ *  it under the terms of the GNU General Public License as published by    *
  *  the Free Software Foundation, either version 3 of the License, or       *
  *  any later version.                                                      *
  *                                                                          *
  *  This project is distributed in the hope that it will be useful,         *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- *  MERCHANTABILITY or FITNESS FOR Individual PARTICULAR PURPOSE.  See the           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
  *  GNU General Public License for more details.                            *
  *                                                                          *
  *  You should have received a copy of the GNU General Public License       *
