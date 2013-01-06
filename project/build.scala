179123c83eee91e554bc2902a455d106148cc10f
Christian Krause <kizkizzbangbang@googlemail.com>
2013-01-06 12:35:40 +0100

    updates graph dependency version

--- project/build.scala
+++ project/build.scala
@@ -100,7 +100,7 @@ object eva4s extends Build {
 
 object Dependencies {
   lazy val chart  = "com.github.wookietreiber.sfreechart" %% "sfreechart"        % "0.1.0"
-  lazy val graph  = "com.assembla.scala-incubator"        %  "graph-core_2.10.0-RC5" % "1.5.2"
+  lazy val graph  = "com.assembla.scala-incubator"        %% "graph-core"        % "1.6.0"
   lazy val extras = "com.github.scala-collection-extras"  %% "collection-extras" % "latest.integration"
   lazy val scalaz = "org.scalaz"                          %% "scalaz-core"       % "7.0.0-M7"
 }
