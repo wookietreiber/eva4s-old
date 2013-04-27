b984d05d5adb16eab45ea42134e56dfb68c71d10
Christian Krause <kizkizzbangbang@googlemail.com>
2013-04-27 12:40:38 +0200

    improved Individual documentation

--- core/src/main/scala/Individual.scala
+++ core/src/main/scala/Individual.scala
@@ -24,8 +24,27 @@
 
 package org.eva4s
 
-/** Represents an individual with its genome and fitness.
+/** Represents a candidate solution of a problem and its fitness.
   *
-  * @tparam Genome the type of the genome of the individuals
+  * == Genome ==
+  *
+  * Individuals carry genetic information, the [[Individual!.genome genome]]. The genome directly
+  * represents a candidate solution to a [[Evolutionary!.problem problem]].
+  *
+  * == Fitness ==
+  *
+  * The [[Individual!.fitness fitness]] of an individual, in biology, describes its ability to both
+  * survive and reproduce. In the context of evolutionary algorithms it serves as a value describing
+  * how optimal a candidate solution is for solving a given [[Evolutionary!.problem problem]]. The
+  * fitness of an individual is used by both [[Selection environmental selection]] and
+  * [[Matchmaking parental selection]].
+  *
+  * @tparam G the type of the genome of the individuals
+  *
+  * @param genome Returns the genome of this individual.
+  * @param fitness Returns the fitness of this individual.
+  *
+  * @todo separation of genotype and phenotype
+  * @todo generic fitness type ([[scala.math.Numeric Numeric]], [[scala.math.Ordering Ordering]])
   */
-case class Individual[Genome](genome: Genome, fitness: Double)
+case class Individual[G](genome: G, fitness: Double)
